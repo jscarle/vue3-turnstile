039d4fb0b696812d9c724328ba2c4f36fd7cafd5
Jean-Sebastien Carle <29762210+jscarle@users.noreply.github.com>
2025-07-12 22:47:41 -0400

    Add detailed env variable typings (#5)

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,30 @@
 /// <reference types="vite/client" />
+
+import type {
+  AppearanceMode,
+  FailureRetryMode,
+  RefreshExpiredMode,
+  RefreshTimeoutMode,
+  Theme,
+  WidgetSize,
+} from '@/types/turnstile';
+import type { TurnstileLanguage } from '@/TurnstileWidget.vue';
+
+interface ImportMetaEnv {
+  readonly VITE_TURNSTILE_SITEKEY?: string
+  readonly VITE_TURNSTILE_LOGLEVEL?: 'debug' | 'info' | 'warn' | 'error'
+  readonly VITE_TURNSTILE_THEME?: Theme
+  readonly VITE_TURNSTILE_LANGUAGE?: TurnstileLanguage
+  readonly VITE_TURNSTILE_TABINDEX?: number
+  readonly VITE_TURNSTILE_SIZE?: WidgetSize
+  readonly VITE_TURNSTILE_RETRY?: FailureRetryMode
+  readonly VITE_TURNSTILE_RETRY_INTERVAL?: number
+  readonly VITE_TURNSTILE_REFRESH_EXPIRED?: RefreshExpiredMode
+  readonly VITE_TURNSTILE_REFRESH_TIMEOUT?: RefreshTimeoutMode
+  readonly VITE_TURNSTILE_APPEARANCE?: AppearanceMode
+  readonly VITE_TURNSTILE_FEEDBACK_ENABLED?: boolean
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
