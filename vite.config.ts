469ca5d60890a001bf2979f29b1c086d0211b15c
Jean-Sebastien Carle <29762210+jscarle@users.noreply.github.com>
2025-07-12 20:18:14 -0400

    build: prepare open-source package

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,28 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    lib: {
+      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+      name: 'Vue3Turnstile',
+      fileName: (format) => `vue3-turnstile.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue', '@unhead/vue'],
+      output: {
+        exports: 'named',
+      },
     },
   },
 })
